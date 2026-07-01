c3c0027927bb44aee9083da4af9739cf17174aa7
Sourabh <sourabh@Sourabhs-MacBook-Air.local>
2026-07-01 10:20:08 +0530

    feat: router setup and global styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Board from './pages/Board'
+
 export default function App() {
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/boards/:id" element={<Board />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
